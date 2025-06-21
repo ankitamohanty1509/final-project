4d6243a59cbb132b817cb2b74c868dbbd2ccd424
Juhi Choudhary <115893427+Juhi5863@users.noreply.github.com>
2025-06-21 20:34:19 +0530

    Delete frontend/script.sh

--- frontend/script.sh
+++
@@ -1,3 +0,0 @@
-# docker build -t inventory-frontend .
-# docker tag inventory-frontend:latest 713881797197.dkr.ecr.us-east-1.amazonaws.com/inventory-frontend
-# docker push 713881797197.dkr.ecr.us-east-1.amazonaws.com/inventory-frontend
